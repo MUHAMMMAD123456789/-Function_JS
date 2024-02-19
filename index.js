72470af7e45f77d1ae811210a99a5569115eb3ac
Ali <daminovmuhammadali3@gmail.com>
2024-02-19 17:32:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,133 @@
+// let name = "Alex"
+// let password = 7777
+// let money = 10000
+
+// let user_name = prompt("what is ur name").toLowerCase().trim()
+
+// if(user_name  === name.toLowerCase()) {
+//     let user_pwd = +prompt("Password")
+
+//     if(user_pwd = password) {
+//         let cashout = +prompt("How much to withdraw")
+
+//         if(cashout <= money && cashout > 0){
+//             alert(`
+//                 User: ${name}
+//                 Current balance ${money - cashout}
+//                 Before withdraw ${money}
+//                 Taken out: ${cashout} 
+//             `)
+//         } else {
+//             alert("not enough sources")
+//         }
+//     } else{
+//         alert("Incorrect password")
+//     }
+// } else{
+//     alert("User name is not found")
+// }
+
+
+
+
+
+
+
+
+// let letter = 'a'
+// let min_age = 20
+// let max_age = 40
+// let money = 100
+
+
+// let user_name = prompt(`Ваше имя?`).toLowerCase().trim()
+
+// if(user_name[0] === letter) {
+//     let user_age = prompt("Сколько вам лет?")
+
+//     if(user_age >= min_age && user_age <= max_age){
+//         let user_money = prompt("Сколько денег у вас?")
+
+//         if(user_money >= money){
+//             alert("Welcome")
+//         } else{
+//             alert("Not enough resources")
+//         }
+//     } else{
+//         alert("Age is not suitable")
+//     }
+// } else {
+//     alert("People with the first letter A in their name")
+// }
+
+
+
+
+
+
+
+
+// let guest_list = "putin biden hitler trump"
+
+// let name = prompt("what is your name mr president")
+
+// if(name === "putin") {
+//     alert("Добро пожаловать")
+// } else if (name === "biden") {
+//     alert("Welcome") 
+// } else if(name === "hitler") {
+//     alert("Heil Hitler")
+// } else if (name === "trump") {
+//     alert("Come in")
+// } else {
+//     alert("who you " + name)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let day = +prompt("which day of the month?")
+
+// if(day >= 1 && day <= 10){
+//     alert("First decade")
+// } else if (day >= 11 && day <= 20) {
+//     alert("Second decade")
+// } else if (day >= 21 && day <= 31) {
+//     alert("Third decade")
+// } else {
+//     alert("Error")
+// }
+ 
+
+
+
+
+
+// let season = +prompt("which month?")
+
+// if (season >= 1 && season <= 2 || season === 12) {
+//       alert ("Winter")
+// }   else if ( season >= 3 && season <= 5) {
+//       alert ("Spring")
+// }   else if  ( season >= 6 && season <= 8)  {
+//       alert ("Summer")
+// }   else if ( season >= 9 && season <= 11) {
+//       alert ( "Autumn")
+// }   else {
+//     alert  ("Error") 
+// }
+
+
+
+
+
+
+
